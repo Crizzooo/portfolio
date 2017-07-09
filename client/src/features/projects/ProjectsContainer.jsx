b27ea78661a1053044518b5b1d3017858b826d23
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-09 00:42:07 -0400

    feat (init project section setup)

--- client/src/features/projects/ProjectsContainer.jsx
+++ client/src/features/projects/ProjectsContainer.jsx
@@ -4,9 +4,30 @@ import React from 'react';
 import './ProjectsStylesheet.scss';
 
 const ProjectsContainer = () => (
-  <div className="projectsContainer">
+  <div className="projectSection">
+    <div className="projectContainer">
+      <div className="row triangleRow">
+        <div className="col-xs-12 projectsTriangleRow">
+        </div>
+      </div>
+      <div className="row contentRow">
+        <div className="col-xs-12 content">
+        </div>
+      </div>
+      <div className="row bottomTriangle">
+        <div className="col-xs-12 projectsTriangleRow">
+        </div>
+      </div>
+    </div>
   </div>
 );
 
 
 export default ProjectsContainer;
+
+// <div className="two">
+//   <div className="right">
+//     2 adsasodasdasd asdasdasdasd asdasdas
+//   </div>
+// </div>
+// <div className="three">3</div>
