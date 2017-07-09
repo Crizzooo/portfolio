e5fd38b411af5d9309f28fcb25eafed4f9cb168f
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-09 12:53:51 -0400

    feat (initial sketch of contact section complete and basic responsive)

--- client/src/features/contact/ContactContainer.jsx
+++ client/src/features/contact/ContactContainer.jsx
@@ -4,8 +4,42 @@ import React from 'react';
 import './ContactStylesheet.scss';
 
 const ContactContainer = () => (
-  <div className="contactContainer">
-    <h1>Contact</h1>
+  <div className="start-xs contactContainer">
+    <div className="rowContainer">
+      <div className="row">
+        <div className="col-xs-12 contactHeader">
+          Contact
+        </div>
+      </div>
+      <div className="row center-xs">
+        <div className="col-xs-10 around-xs contentHolder">
+          <div className=" col-xs-12 col-lg-3 contactCard">
+            <div className="contactImageHolder">
+            </div>
+            <div className="contactImageCaption">
+            </div>
+          </div>
+          <div className=" col-xs-12 col-lg-3 contactCard">
+            <div className="contactImageHolder">
+            </div>
+            <div className="contactImageCaption">
+            </div>
+          </div>
+          <div className=" col-xs-12 col-lg-3 contactCard">
+            <div className="contactImageHolder">
+            </div>
+            <div className="contactImageCaption">
+            </div>
+          </div>
+          <div className=" col-xs-12 col-lg-3 contactCard">
+            <div className="contactImageHolder">
+            </div>
+            <div className="contactImageCaption">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
