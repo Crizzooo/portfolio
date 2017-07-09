e27c90af70372b775902192e80864c6b2cb2120c
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-09 13:50:46 -0400

    feat (added footer, content, and pulsing green heart)

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import HeroContainer from './features/hero/HeroContainer.jsx';
 import ProjectsContainer from './features/projects/ProjectsContainer.jsx';
 import ContactContainer from './features/contact/ContactContainer.jsx';
+import Footer from './features/footer/';
 
 //load components
 const App = () => (
@@ -11,6 +12,7 @@ const App = () => (
     <HeroContainer />
     <ProjectsContainer />
     <ContactContainer />
+    <Footer />
   </div>
 );
 
