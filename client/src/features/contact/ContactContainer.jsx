3f3a283f7c1c21eb1ac3cfc7cd25a08f87032bae
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-09 11:37:57 -0400

    feat (add ContactContainer)

---
+++ client/src/features/contact/ContactContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//load components
+import './ContactStylesheet.scss';
+
+const ContactContainer = () => (
+  <div className="contactContainer">
+    <h1>Contact</h1>
+  </div>
+);
+
+
+export default ContactContainer;
