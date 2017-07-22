bc2ceb392b493b3a29e65c790d02092dec5ff3d1
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-22 13:11:28 -0400

    feat (init contact card placement, style, and resume button no longer opens new tab)

--- client/src/features/contact/ContactCard.jsx
+++ client/src/features/contact/ContactCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import MediumSVG from './MediumSVG.jsx';
+import GmailSVG from './GmailSVG.jsx';
+import GithubSVG from './GithubSVG.jsx';
+
 const ContactCard = () => (
   <div className=" col-xs-12 col-lg-3 contactCard">
     <div className="contactImageHolder">
+      <div className="svgHolder">
+        <MediumSVG />
+      </div>
     </div>
     <div className="contactImageCaption">
+      <span>image info goes here</span>
     </div>
   </div>
 );
