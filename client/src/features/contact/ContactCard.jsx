ed3ae3d7ab7ef33f6456df2cc2e988e6b6b797e1
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-23 14:22:08 -0400

    feat (contact card is now smart class)

--- client/src/features/contact/ContactCard.jsx
+++ client/src/features/contact/ContactCard.jsx
@@ -5,43 +5,52 @@ import GmailSVG from './GmailSVG.jsx';
 import GithubSVG from './GithubSVG.jsx';
 import LinkedInSVG from './LinkedInSVG.jsx';
 
-const ContactCard = (props) => {
-  return (
-  <div className="col-xs-5 col-sm-4 col-lg-3 contactCard">
-    <div className="row center-xs middle-xs">
-      <div className="col-xs-12 contactImageHolder">
-        <div className="svgHolder">
-          { generateIcon(props.type)}
+
+export default class ContactCard extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      hover: false
+    }
+    this.generateIcon = this.generateIcon.bind(this);
+  }
+
+  render(){
+    return (
+      <div className="col-xs-5 col-sm-4 col-lg-3 contactCard">
+        <div className="row center-xs middle-xs">
+          <div className="col-xs-12 contactImageHolder">
+            <div className="svgHolder">
+              { this.generateIcon(this.props.type)}
+            </div>
+          </div>
+          <div className="col-xs-12 contactImageCaption">
+            <span>{this.props.desc}</span>
+          </div>
         </div>
       </div>
-      <div className="col-xs-12 contactImageCaption">
-        <span>{props.desc}</span>
-      </div>
-    </div>
-  </div>
-  );
-};
-
-
-//TODO: Rewrite so that on mouse enter, icon begins to spin and text fades down 
-
-export default ContactCard;
-
-function generateIcon(type) {
-  let icon;
-  switch (type) {
-    case 'github':
-      icon = (<GithubSVG />);
-      break;
-    case 'medium':
-      icon = (<MediumSVG />);
-      break;
-    case 'linkedin':
-      icon = (<LinkedInSVG />);
-      break;
-    default:
-      icon = (<GmailSVG />);
-
+    );
   }
-  return icon;
+
+  generateIcon(type){
+    let icon;
+    switch (type) {
+      case 'github':
+        icon = (<GithubSVG />);
+        break;
+      case 'medium':
+        icon = (<MediumSVG />);
+        break;
+      case 'linkedin':
+        icon = (<LinkedInSVG />);
+        break;
+      default:
+        icon = (<GmailSVG />);
+    }
+    return icon;
+  }
 }
+
+
+//TODO: Rewrite so that on mouse enter, icon begins to spin and text fades down
