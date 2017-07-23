a32e8630b7cf3569f1a8d52c7601a80bd129750e
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-22 20:22:16 -0400

    feat (added initial images to project cards)

--- client/src/features/projects/ProjectCard.jsx
+++ client/src/features/projects/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card, CardHeader, CardMedia, CardTitle, CardText, CardActions} from 'material-ui/card';
 import Icon from './Icon.jsx';
+import AnimatedButton from 'react-animated-button';
 
 export default class ProjectCardControlled extends React.Component {
 
@@ -9,6 +10,7 @@ export default class ProjectCardControlled extends React.Component {
     this.state = {
       expanded: false,
     };
+    this.generateImage = this.generateImage.bind(this);
   }
 
   handleExpandChange = (expanded) => {
@@ -31,9 +33,7 @@ export default class ProjectCardControlled extends React.Component {
                     "borderBottomLeftRadius": (this.state.expanded ? "0px" : "2px"),
                     "borderBottomRightRadius": (this.state.expanded ? "0px" : "2px")}}
           >
-            <img src={ this.props.imgSrc ? this.props.imgSrc : "/assets/heroBackground20003000.jpg"}
-                 style={{"height":"300px", "transform":"scale(1.15)"}}
-              />
+          { this.generateImage() }
           </CardMedia>
           <CardText expandable={true} style={{"backgroundColor":"rgb(33, 33, 33)", "color":"white"}}>
             <p>{ this.props.desc }</p>
@@ -54,5 +54,23 @@ export default class ProjectCardControlled extends React.Component {
           </CardActions>
         </Card>
     </div>);
+  }
+
+  generateImage() {
+    if (this.props.title === 'react-animated-button'){
+      console.log('button!');
+      return (
+        <div style={{"backgroundImage":"url(/assets/heroBackground20003000.jpg)", "height":"300px","transform":"scale(1.15)", "backgroundSize":"contain"}}>
+          <div className="buttonProjOverlay">
+            <AnimatedButton />
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <img src={ this.props.imgSrc ? this.props.imgSrc : "/assets/heroBackground20003000.jpg"}
+          style={{"height":"300px", "transform":"scale(1.15)"}}
+          />)
+    }
   }
 }
