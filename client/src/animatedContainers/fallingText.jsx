3ecf608068ef129012dac684646886ede4498c75
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-24 16:24:55 -0400

    feat (falling text now occurs and then button animates, fix state rerenders)

--- client/src/animatedContainers/fallingText.jsx
+++ client/src/animatedContainers/fallingText.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-
-import FallingLetter from './fallingLetter.jsx';
+import Anime from 'react-anime';
 
 const FallingText = (props) => {
+  if (props.shouldAnimate) {
+    return(
+      <Anime
+        translateY={[-1000, 0]}
+        duration={ (el, i, l) => 3000 }
+        delay={ (el, i) => 2000 + 100 * i}
+        elasticity={ (el, i) => 100 + i * 20}
+        complete={ props.onComplete ? props.onComplete : null}
+        easing="easeOutBack"
+        >
+        {props.text.split('').map( (el) => createLetter(el))}
+      </Anime>
+    );
+  } else {
+    return (
+      <span>
+        {props.text.split('').map( (el) => createLetter(el))}
+      </span>
+    )
+  }
+}
+
+const createLetter = (letter) => {
   return(
-  <div className="">
-    { props.text &&
-      props.text.split('').map( (elem, i) => <FallingLetter letter={elem} index={i} /> ) }
-  </div>
-);
+    <div className="letter">
+    {letter}
+  </div>);
 }
 
 export default FallingText;
--- client/src/features/hero/heroContainer.jsx
+++ client/src/features/hero/heroContainer.jsx
@@ -11,12 +11,24 @@ export default class HeroContainer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      mounted: false
+      mounted: false,
+      shouldAnimate: true,
+      showButton: false
     };
+
+    this.animationComplete = this.animationComplete.bind(this);
   }
 
   componentDidMount(){
-    this.setState({mounted: true});
+    this.setState({mounted: true, showButton: false});
+    // setTimeout( () => {
+    //   console.log('turning on button..')
+    //   this.setState({ showButton: true});
+    // }, 2600);
+  }
+
+  animationComplete(){
+    this.setState({showButton: true, shouldAnimate: false});
   }
 
   render() {
@@ -27,16 +39,20 @@ export default class HeroContainer extends React.Component {
         <div className="textContainer">
             <div className="row center-xs">
               <div className="col-xs-7 col-sm-4 col-lg-2 col-xl-1 left">
-                { this.state.mounted ? <FallingText text="Chris" /> : null }
+                { this.state.mounted ? <FallingText text="Chris" shouldAnimate={this.state.shouldAnimate} /> : null }
               </div>
             </div>
             <div className="row center-xs">
               <div className="col-xs-7 col-sm-4 col-lg-2 col-xl-1 right">
-                { this.state.mounted ? <FallingText text="Rizzo" /> : null }
+                { this.state.mounted ? <FallingText text="Rizzo" shouldAnimate={this.state.shouldAnimate} onComplete={ this.animationComplete } /> : null }
               </div>
             </div>
             <div className="row center-xs">
-              <a className="resumeLink" href="/resume"><AnimatedButton strokeColor="beige" buttonText="view resume" containerStyle={{"color":"beiege"}}/></a>
+              { this.state.showButton ?
+                <a className="resumeLink animated bounce" href="/resume"><AnimatedButton strokeColor="beige" buttonText="view resume" containerStyle={{"color":"beiege"}}/></a>
+                :
+                <a className="resumeLink hidden" href="/resume"><AnimatedButton strokeColor="beige" buttonText="view resume" containerStyle={{"color":"beiege"}}/></a>
+              }
             </div>
         </div>
       </div>
