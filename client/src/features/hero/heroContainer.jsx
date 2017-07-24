d57d76cb0ef0e2601d7dc5c427b4cac6480cd476
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-24 19:15:01 -0400

    fix (restructure names to use padding instead of columns for overlap - same position on all screen sizes)

--- client/src/features/hero/heroContainer.jsx
+++ client/src/features/hero/heroContainer.jsx
@@ -32,28 +32,22 @@ export default class HeroContainer extends React.Component {
       <div className="heroContainer">
         <div className="heroPic">
         </div>
-        <div className="textContainer">
-            <div className="row center-xs">
-              <div className="col-xs-7 col-sm-4 col-lg-2 col-xl-1 left quarto">
-                <div>
-                { this.state.mounted ? <FallingText text="CHRIS" shouldAnimate={this.state.shouldAnimate} /> : null }
-                </div>
+        <div className="row center-xs textContainer quarto">
+            <div className="col-xs-12 row center-xs">
+              <div className="padRight">
+              { this.state.mounted ? <FallingText text="CHRIS" shouldAnimate={this.state.shouldAnimate} /> : null }
               </div>
             </div>
-            <div className="row center-xs">
-              <div className="col-xs-7 col-sm-4 col-lg-2 col-xl-1 right quarto lastName">
-                <div>
-                  { this.state.mounted ? <FallingText text="RIZZO" shouldAnimate={this.state.shouldAnimate} onComplete={ this.animationComplete } /> : null }
-                </div>
+            <div className="col-xs-12 row center-xs lastName">
+              <div className="padLeft">
+              { this.state.mounted ? <FallingText text="RIZZO" shouldAnimate={this.state.shouldAnimate} onComplete={ this.animationComplete } /> : null }
               </div>
             </div>
-            <div className="row center-xs">
-              { this.state.showButton ?
-                <a className="resumeLink animated fadeInDown" href="/resume"><AnimatedButton strokeColor="beige" buttonText="view resume" containerStyle={{"color":"beiege"}}/></a>
-                :
-                <a className="resumeLink hidden" href="/resume"><AnimatedButton strokeColor="beige" buttonText="view resume" containerStyle={{"color":"beiege"}}/></a>
-              }
-            </div>
+            { this.state.showButton ?
+              <a className="resumeLink animated fadeInDown" href="/resume"><AnimatedButton strokeColor="beige" buttonText="view resume" containerStyle={{"color":"beiege"}}/></a>
+              :
+              <a className="resumeLink hidden" href="/resume"><AnimatedButton strokeColor="beige" buttonText="view resume" containerStyle={{"color":"beiege"}}/></a>
+            }
         </div>
       </div>
     );
