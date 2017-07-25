af9b954da3f78daf9d3b679ffb0cd263b133b5d7
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-25 12:18:34 -0400

    fix (added start-prod script and Procfile to run start-prod for heroku)

--- server/index.js
+++ server/index.js
@@ -1,21 +1,12 @@
 /*eslint-disable camelcase*/
 const path = require('path');
 const express = require('express');
-const volleyball = require('volleyball');
-const bodyParser = require('body-parser');
 const fs = require('fs');
 
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-// logging middleware
-app.use(volleyball);
-
-// body parsing middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
 
 
 app.listen(PORT, () => {
